763170190079c0e2a44e804836af9e5087da5f22
Mehmet Akbudak <mehmet.akbudak@ecozum.com>
2021-09-14 14:25:15 +0300

    Add project files.

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Factory Method *************
+            Amaç: Oluşturulmak istenen sınıfın kendisinden bir örnek istemek yerine Factory Metod patterni sayesinde tek bir instance üzerinden gerekli nesnenin üretilmesini sağlamaktır.
+              
+            Algoritmada belirli adımlarda farklı tiplerde nesnelere ihtiyaç olabilir. Bu nesneleri if-else veyahut switch yapıları aracılığıyla duruma göre oluşturabilirsiniz. Tabi bunu her seferinde yapmanın zaman ve kod açısından çok tasarruflu olduğu düşünülemez. İşte böyle durumlarda Factory Metod patterni kullanılabilir.
+
+            Bu işlemi gerçekleştirmek için abstract class ya da interface yapıları kullanılır. Bu yapılardan faydalanmamızın sebebi bu yapılardan kalıtım alan sınıfın abstract ya da interface içerisinde elemanı mecbur uygulamak zorunda olmasıdır. Ama daha mühümi farklı tipteki nesneleri oluşturmakla ilgilenmek zorunda kalmayacak olan Base Class'ımızın(ki bu sınıftan oluşturulmasını istediğimiz nesneleri türeteceğiz) oluşturduğumuz nesneyi Polymorpism(çok biçimlilik) aracılığı ile üst bir referans türü ile işaretleyebilmektir.
+
+            */
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ TaskAsyncAwait/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskAsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t = Task.Run(() =>
+             {
+                 Console.WriteLine("Merhaba Dünya!");
+                 Thread.Sleep(2000);
+                 Console.WriteLine("Merhaba Galaksi'ler");
+             });
+            Console.WriteLine(t.IsCompleted);
+            Console.WriteLine("Merhaba Mars!");
+            Thread.Sleep(3000);
+            Console.WriteLine(t.IsCompleted);
+        }
+    }
+}
