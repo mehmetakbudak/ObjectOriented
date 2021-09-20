6533ff2a1fc2b5b9743d771d128b98a982272bfd
Mehmet Akbudak <akbudak.mehmet@gmail.com>
2021-09-20 18:50:27 +0300

    ...

---
+++ ConsumerApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace ConsumerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("mesajkuyrugu", false, false, true);
+                EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                channel.BasicConsume("mesajkuyrugu", false, consumer);
+                consumer.Received += (sender, e) =>
+                 {
+                     var body = e.Body.ToArray();
+                     Console.WriteLine(Encoding.UTF8.GetString(body));
+                 };
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ PublisherApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace PublisherApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Bağlantı tanımlanıyor
+            ConnectionFactory factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+
+            //Bağlantı sağlanmakta
+            using (IConnection connection = factory.CreateConnection())
+            //Kanal oluşturulmaktadır
+            using (IModel channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("mesajkuyrugu", false, false, true);
+                byte[] byteMessage = Encoding.UTF8.GetBytes("Merhaba RabbitMQ");
+                channel.BasicPublish(exchange: "", routingKey: "mesajkuyrugu", body: byteMessage);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
